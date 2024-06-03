1a90b14bd76b4b5a19dbcc4b90b55f0e6328edb4
DawID-Log <dawid.logoluso@gmail.com>
2024-06-03 22:39:34 +0200

    [MAIN] added multi port and px argoment to set

--- src/Server.cs
+++ src/Server.cs
@@ -1,74 +1,139 @@
 using System.Net;
+using System.Net.Http;
 using System.Net.Sockets;
+using System.Runtime.Caching;
 using System.Text;
 
-Dictionary<string, string> getAndSet = new Dictionary<string, string>();
+var db = MemoryCache.Default;
 const string crlf = "\r\n";
-using var server = new TcpListener(IPAddress.Any, 6379);
+const int accountForEnding = 2;
+int port = 6379;
+
+if (args.Length == 2 && args[0].ToUpper() == "--PORT") {
+  port = int.Parse(args[1]);
+  Console.WriteLine($"{port}");
+}
+TcpListener server = new TcpListener(IPAddress.Any, port);
+
+var ParseLength = (byte[] bytes, int start) => {
+  int len = 0;
+  int i = start + 1;
+  int consumed = 1;
+
+  // 0x0D is '\r' hex value
+  while (bytes[i] != 0x0D) {
+    len = (len * 10) + (bytes[i] - '0');
+    i++;
+    consumed++;
+  }
+
+  return (len, consumed + accountForEnding);
+};
+
+var ParseBulkStr = (Byte[] bytes, int start) => {
+  var (len, consumed) = ParseLength(bytes, start);
+  Byte[] payload = new Byte[len];
+  Array.Copy(bytes, start + consumed, payload, 0, len);
+  var parsed = Encoding.ASCII.GetString(payload);
+
+  return (parsed, consumed + len + accountForEnding);
+};
+
+var ParseClientMsg = (Byte[] msg) => {
+  var (bulkStrsToParse, cursor) = ParseLength(msg, 0);
+  String bulkStr = "";
+
+  for (int i = 0; i < bulkStrsToParse; i++) {
+    var (str, consumed) = ParseBulkStr(msg, cursor);
+    bulkStr += (str + " ");
+    cursor += consumed;
+  }
+  bulkStr = bulkStr.TrimEnd();
+  return bulkStr;
+};
+
+var ProcessPing = (string inputCmd) => {
+  return inputCmd.ToLower() switch {
+    "ping" => "+PONG\r\n",
+    _ => string.Format("${0}\r\n{1}\r\n", inputCmd.Length - 5, inputCmd.Substring(5)),
+  };
+};
+
+var ProcessEcho = (string inputCmd) => {
+  return string.Format("${0}\r\n{1}\r\n", inputCmd.Length - 5, inputCmd.Substring(5));
+};
+
+var ProcessSet = (string inputCmd) => {
+  var parts = inputCmd.Split(' ');
+  var valueToBeSave = (object)parts[2];
+  if (parts[0] != "set") {
+    throw new ArgumentException("Expected: set <k> <v> [px <t>], but got {0}", inputCmd);
+  } else if (parts.Length > 5 || parts[parts.Length - 2] != "px") {
+    // -4: length of the array substring, starting at index 2 and ending at index parts.Length - 2.
+    int length = parts.Length - 2;
+    if(parts[parts.Length - 2] == "px"){
+      length -= 2;
+    }
+    valueToBeSave = (object)string.Join(" ", parts, 2, length);
+  }
+
+  if (parts[parts.Length - 2] == "px") {
+    var ttl = int.Parse(parts[parts.Length - 1]);
+    db.Set(parts[1], valueToBeSave, DateTimeOffset.Now.AddMilliseconds(ttl));
+  } else {
+    db.Set(parts[1], valueToBeSave, DateTimeOffset.MaxValue);
+  }
+  return "+OK\r\n";
+};
+
+var ProcessGet = (string inputCmd) => {
+  var parts = inputCmd.Split(" ");
+
+  if (parts.Length != 2 || parts[0] != "get") {
+    throw new ArgumentException("Expected: get <k>, but got {0}", inputCmd);
+  }
+  string? val = db[parts[1]] as string;
+
+  if (val == null) {
+    return "$-1\r\n";
+  }
+  
+  return String.Format("${0}\r\n{1}\r\n", val.Length, val);
+};
+
+var HandleClient = (TcpClient client) => {
+  NetworkStream stream = client.GetStream();
+  byte[] rxBuffer = new byte[1024];
+
+  while (true) {
+    int bytesResponse = stream.Read(rxBuffer);
+    if (bytesResponse == 0)
+      return;
+    if (bytesResponse < rxBuffer.Length) {
+      Array.Resize(ref rxBuffer, bytesResponse);
+    }
+    String command = ParseClientMsg(rxBuffer);
+
+    var response = command.ToLower() switch {
+      var input when input.StartsWith("ping") => ProcessPing(input),
+      var input when input.StartsWith("echo") => ProcessEcho(input),
+      var input when input.StartsWith("set") => ProcessSet(input),
+      var input when input.StartsWith("get") => ProcessGet(input),
+      _ => throw new ArgumentException(
+          string.Format("Received unknown redis command: {0}", command)),
+    };
+    byte[] bytes = Encoding.ASCII.GetBytes(response);
+    stream.Write(bytes);
+  }
+};
 
 Console.WriteLine("Logs from your program will appear here!");
 try {
   server.Start();
   while (true) {
-    Socket socket = server.AcceptSocket(); // wait for client
-    _ = Task.Run(() => PongResponder(socket));
+    TcpClient tcpClient = await server.AcceptTcpClientAsync();
+    new Thread(() => HandleClient(tcpClient)).Start();
   }
-} finally {
-  server.Stop();
-}
-
-
-
-async Task PongResponder(Socket socket) {
-  while (socket.Connected) {
-    byte[] rxBuffer = new byte[1024];
-    var Chars = new char[1024];
-    int bytesReceived = await socket.ReceiveAsync(rxBuffer);
-
-    Console.WriteLine($"Received {bytesReceived} bytes.");
-    
-    if(bytesReceived > 0) {
-      string data = Encoding.ASCII.GetString(rxBuffer, 0, bytesReceived);
-      var request = data.Split(crlf, StringSplitOptions.RemoveEmptyEntries);
-
-      if (request.Length > 0) {
-        var parsingReq = await HandleParsing(request);
-        await socket.SendAsync(Encoding.ASCII.GetBytes(parsingReq));
-      } 
-    } else {
-      socket.Close();
-    }
-  }
-}
-
-async Task<string> HandleParsing(string[] request) {
-  string RespSimpleString(string input) => $"+{input}{crlf}";
-  string reply = "-ERR unknown command\r\n";
-  string cmd = request[2];
-  string argsReq = request[4];
-
-  switch (cmd.ToLower()) {
-    case "ping":
-      reply = RespSimpleString("PONG");
-      break;
-    case "echo":
-      reply = $"${argsReq.Length}\r\n{argsReq}\r\n";
-      break;
-    case "set":
-      string valueToSet = request[6];
-      getAndSet[argsReq] = valueToSet;
-      Console.WriteLine($"getAndSet[argsReq]: {getAndSet[argsReq]}");
-      reply = "+OK\r\n";
-      break;
-    case "get":      
-      if (getAndSet.ContainsKey(argsReq)) {
-        var valueToGet = getAndSet[argsReq];
-        Console.WriteLine($"valueToGet?: {valueToGet}");
-        reply = $"${valueToGet.Length}\r\n{valueToGet}\r\n";
-      } else {
-        reply = "$-1\r\n";
-      }
-      break;
-  }
-  return reply;
+} catch (Exception ex) {
+  Console.WriteLine(ex.Message);
 }
--- test/get_set_redis.sh
+++ test/get_set_redis.sh
@@ -1,8 +1,21 @@
-redis-cli SET foo "valore salvato"
-redis-cli GET foo
+redis-cli SET var "valore salvato"
+redis-cli GET var
+
+redis-cli SET foo "valore salvato" px 2000
+sleep 1 && redis-cli GET foo
 
 valueToShow=10
-redis-cli SET valueNum "valore numerico $valueToShow"
-redis-cli GET valueNum
+redis-cli SET bar "valore numerico $valueToShow" px 1000
+sleep 0.2 && redis-cli GET bar
+sleep 0.2 && redis-cli GET bar
+sleep 0.2 && redis-cli GET bar
+sleep 0.2 && redis-cli GET bar
+echo "After a second"
+sleep 0.2 && redis-cli GET bar
+sleep 0.2 && redis-cli GET bar
 
-# sleep 0.2 && redis-cli GET foo
+
+redis-cli SET foo bar px 400
+sleep 0.2 && redis-cli GET foo
+echo "After two milliseconds"
+sleep 0.2 && redis-cli GET foo
